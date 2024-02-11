7e51cb25dc724dfa5f27d242a8a30b7583005599
Shruti Gajbhiye <shrutigajbhiye2209@gmail.com>
2024-02-11 22:38:10 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Inter } from "next/font/google";
 import HomeHero from "../components/sections/hero";
 import Stats from "@/components/stats";
+import Blog from "@/components/blogs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <><HomeHero /><Stats /></>;
+  return <><HomeHero /><Stats /><Blog /></>;
 }
