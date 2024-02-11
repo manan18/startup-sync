fff64e02c0b40783110e11e157a909b4e3aad838
sumit-022 <lcb2021016@iiitl.ac.in>
2024-02-11 18:33:29 +0530

    Hero Section Updates

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -14,14 +14,13 @@ const HomeHero = () => {
       >
         <source src="/videos/bgvideo.mp4" type="video/mp4" />
       </video>
-      <div className="absolute -z-10 w-full h-full bg-black opacity-70"></div>
+      <div className="absolute -z-10 w-full h-full bg-black opacity-50"></div>
       <div className="flex justify-between p-6 items-center">
         <h1 className="text-white text-2xl">Startup Sync</h1>
         <div className="flex gap-8 items-center justify-center">
           {navLinks.map((link, index) => (
             <Link href={link.path} key={index} className="text-white">
-                  {link.title}
-                  
+              {link.title}
             </Link>
           ))}
         </div>
