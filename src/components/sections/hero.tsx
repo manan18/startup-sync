3559e2d5175bd84bba23643857b683c6461badc3
manan18 <lcb2021017@iiitl.ac.in>
2024-02-11 18:11:29 +0530

    landing1

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -5,27 +5,36 @@ import Button from "../atoms/button";
 
 const HomeHero = () => {
   return (
-    <div className="h-screen relative w-screen overflow-hidden">
-      <video
-        autoPlay
-        muted
-        loop
-        className="w-full absolute -z-20 h-full object-cover"
-      >
-        <source src="/videos/bgvideo.mp4" type="video/mp4" />
-      </video>
-      <div className="absolute -z-10 w-full h-full bg-black opacity-70"></div>
-      <div className="flex justify-between p-6 items-center">
-        <h1 className="text-white text-2xl">Startup Sync</h1>
-        <div className="flex gap-8 items-center justify-center">
-          {navLinks.map((link, index) => (
-            <Link href={link.path} key={index} className="text-white">
-                  {link.title}
-                  
-            </Link>
-          ))}
+    <div className="h-screen relative w-screen overflow-hidden flex flex-col">
+      <div>
+        <video
+          autoPlay
+          muted
+          loop
+          className="w-full absolute -z-20 h-full object-cover"
+        >
+          <source src="/videos/bgvideo.mp4" type="video/mp4" />
+        </video>
+        <div className="absolute -z-10 w-full h-full bg-black opacity-70"></div>
+        <div className="flex justify-between p-6 items-center">
+          <h1 className="text-white text-2xl">Startup Sync</h1>
+          <div className="flex gap-8 items-center justify-center">
+            {navLinks.map((link, index) => (
+              <Link href={link.path} key={index} className="text-white">
+                {link.title}
+              </Link>
+            ))}
+          </div>
+          <Button>Register</Button>
         </div>
-        <Button>Register</Button>
+      </div>
+      <div className="flex-1 flex items-center justify-center flex-col">
+        <div className="my-auto mx-auto w-max h-full">
+          <h1 className="text-white text-4xl flex-col items-center text-center max-w-[60%]">
+            We empower entrepreneurs to transform their ideas into fully
+            functioning startups ""
+          </h1>
+        </div>
       </div>
     </div>
   );
