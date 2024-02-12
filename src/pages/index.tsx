802cae84b96ee97c20e5cb002a275e28dc00c5d3
sakshi2407 <lcs2021020@iiitl.ac.in>
2024-02-12 11:34:06 +0530

    Some mores additions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Inter } from "next/font/google";
 import HomeHero from "../components/sections/hero";
+import CompanySection from "@/components/atoms/company";
+import ContentBox from "@/components/atoms/card";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <HomeHero />;
+  return (
+    <div>
+      <CompanySection />
+      <ContentBox />
+    </div>
+  );
 }
