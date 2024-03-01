7715466e45f47d5962acaf22734e4ce5a4b909d8
sakshi2407 <lcs2021020@iiitl.ac.in>
2024-03-01 21:15:26 +0530

    created navbar and cards for transactions page

--- src/components/transactionsPageComp/TNavbar.tsx
+++ src/components/transactionsPageComp/TNavbar.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { IoMdDownload } from "react-icons/io";
 
 const TNavbar = () => {
   return (
-    <div>
-      <div className="heading flex">
-        <div className="flex flex-col">Expense Summary</div>
+    <div className="flex p-4 justify-between items-center border-b-2 border-gray-300">
+      <div className="heading flex flex-col">
+        <div className="text-3xl mb-2 font-semibold">Expense Summary</div>
         <div>
           Track & manage your spending with ease. Overview of your spending by
           category, document type and more.
         </div>
       </div>
-      <div className="button">
-        <button>Export .CSV</button>
+      <div className="button bg-gray-200 p-3 rounded text-cyan-500 font-semibold flex items-center hover:bg-gray-300 hover: cursor-pointer">
+        <div className="mr-2">
+          <IoMdDownload />
+        </div>
+        <div className="">Export .CSV</div>
       </div>
     </div>
   );
--- src/pages/transactionsPage/index.tsx
+++ src/pages/transactionsPage/index.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import TNavbar from "@/components/transactionsPageComp/TNavbar";
+import Sidebar from "@/components/sidebar";
+import TCard1 from "@/components/transactionsPageComp/TCard1";
 
 const TransactionsPage = () => {
   return (
     <div>
-      <TNavbar />
+      <div className="grid grid-cols-[15%,1fr] h-screen">
+        <div className="">
+          <Sidebar />
+        </div>
+        <div className="bg-gray-100">
+          <div>
+            <TNavbar />
+          </div>
+          <div className="p-5">
+            <div className="mb-5">
+              <TCard1
+                money="$878.24"
+                text1="Total spend this month"
+                text2="15% vs last month"
+              />
+            </div>
+            <div>
+              <TCard1
+                money="$213.68"
+                text1="Payment due this month"
+                text2="1 Unpaid document(s)"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
