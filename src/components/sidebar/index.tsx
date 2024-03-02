90f5320166e297180df285621a83ebbd119c5a0e
ria <lci2021052@iiitl.ac.in>
2024-03-02 18:05:18 +0530

    updated sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,12 +3,12 @@ import sidebarItems1 from "@/data/sidebar/sidebarItems";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col gap-2 p-5">
-      <h1 className="py-[15%] text-center text-2xl font-bold">Startup Sync</h1>
+    <div className="flex flex-col gap-2 p-8">
+      <h1 className="py-[15%] text-center text-3xl font-bold">Startup Sync</h1>
       <div className="main-content flex flex-col gap-2 p-2">
         {sidebarItems1.map((sidebarItem, index) => {
           return (
-            <div className="p-2 rounded-md hover:bg-gray-100 transition-all duration-75 hover:scale-105 text-sm hover:cursor-pointer" key={index}>
+            <div className="p-2 rounded-md hover:bg-gray-100 transition-all duration-75 hover:scale-105 text-md hover:cursor-pointer" key={index}>
               <div className="flex items-center">
                 <div className="mr-3">{sidebarItem.icon}</div>
                 <div>{sidebarItem.title}</div>
