a9093c6ec4b51eb18788037d7122cfabe3bc46d6
manan18 <lcb2021017@iiitl.ac.in>
2024-03-04 17:13:52 +0530

    employeesUpdated

--- src/pages/employees/index.tsx
+++ src/pages/employees/index.tsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import Image from "next/image";
 import { EmployeeTable } from "@/components/employeeTable";
 import employees from "@/data/employees/employees";
 import Sidebar from "@/components/sidebar";
 import { FaChevronLeft } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
+import Dropdown from "@/components/dropdown";
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+import Link from "next/link";
 
 const Employees = () => {
+  const [filteredData, setFilteredData] = useState(employees);
   const [currentPage, setCurrentPage] = useState(1);
   const entriesPerPage: number = 5;
+  const [formData, setFormData] = useState({});
 
   const indexOfLastEntry: number = currentPage * entriesPerPage;
   const indexOfFirstEntry: number = indexOfLastEntry - entriesPerPage;
@@ -19,6 +24,23 @@ const Employees = () => {
   function paginate2(pageNumber: number) {
     setCurrentPage(pageNumber);
   }
+
+  const applyFilters: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const type = formData.getAll("type");
+    const department = formData.getAll("department");
+    const title = formData.getAll("title");
+
+    setFilteredData(
+      data.filter(
+        (item) =>
+          (!type.length || type.includes(item.type)) &&
+          (!department.length || department.includes(item.department)) &&
+          (!title.length || title.includes(item.title))
+      )
+    );
+  };
 
   const [data, setData] = useState(employees);
   const [search, setSearch] = useState("");
@@ -76,12 +98,208 @@ const Employees = () => {
             </div>
           </form>
         </div>
-        <div className="font-bold text-3xl p-5 mx-auto">Employee List</div>
+
+        <div className="flex py-4 mx-auto justify-between px-8">
+          <div className="font-bold text-3xl flex-start">Employee List</div>
+          <div className="flex-end">
+            <Dropdown activeCondition={false}>
+              {(handleClick, open) => (
+                <div className="relative">
+                  <button
+                    type="submit"
+                    className="focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-l p-2 flex border-2"
+                    onClick={handleClick}
+                  >
+                    <HiOutlineAdjustmentsHorizontal className="text-xl cursor-pointer" />
+                    Filter
+                  </button>
+                  {open && (
+                    <div className="absolute top-8 right-0 min-w-[200px] bg-white p-3 rounded-lg py-2 text-gray-500 shadow-md">
+                      <form id="filterForm" onSubmit={applyFilters}>
+                        <ul className="py-2">
+                          <li className="py-2">
+                            <h2 className="text-gray-500 font-medium">
+                              User Type
+                            </h2>
+                            <ul className="px-3">
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="type"
+                                  value="Admin"
+                                  id="default-checkbox"
+                                />
+                                Admin
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="type"
+                                  value="HR"
+                                  id=""
+                                />
+                                HR
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="type"
+                                  value="Employee"
+                                  id=""
+                                />
+                                Employee
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="type"
+                                  value="Accounting"
+                                  id=""
+                                />
+                                Accounting
+                              </li>
+                            </ul>
+                          </li>
+
+                          <li className="py-2">
+                            <h2 className="text-gray-500 font-medium">
+                              Department
+                            </h2>
+                            <ul className="px-3">
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="department"
+                                  value="HR"
+                                  id="default-checkbox"
+                                />
+                                HR
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  id=""
+                                  name="department"
+                                  value="Payroll"
+                                />
+                                Payroll
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="department"
+                                  value="Management"
+                                  id=""
+                                />
+                                Management
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="department"
+                                  value="Recruitment"
+                                  id=""
+                                />
+                                Recruitment
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="department"
+                                  value="IT Support"
+                                  id=""
+                                />
+                                IT Support
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="department"
+                                  value="Finance"
+                                  id=""
+                                />
+                                Finance
+                              </li>
+                            </ul>
+                          </li>
+
+                          <li className="py-2">
+                            <h2 className="text-gray-500 font-medium">
+                              Job Title
+                            </h2>
+                            <ul className="px-3">
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="title"
+                                  value="Director"
+                                  id="default-checkbox"
+                                />
+                                Director
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="title"
+                                  value="Manager"
+                                  id=""
+                                />
+                                Manager
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="title"
+                                  value="Consultant"
+                                  id=""
+                                />
+                                Consultant
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="title"
+                                  value="Accountant"
+                                  id=""
+                                />
+                                Accountant
+                              </li>
+                              <li>
+                                <input
+                                  type="checkbox"
+                                  name="title"
+                                  value="IT"
+                                  id=""
+                                />
+                                IT Technician
+                              </li>
+                            </ul>
+                          </li>
+
+                          <li className="py-2">
+                            <div className="flex justify-center">
+                              <button
+                                type="submit"
+                                className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                              >
+                                Apply Filters
+                              </button>
+                            </div>
+                          </li>
+                        </ul>
+                      </form>
+                    </div>
+                  )}
+                </div>
+              )}
+            </Dropdown>
+          </div>
+        </div>
 
         <div className="max-w-[90%] mx-auto">
           <div className="flex flex-col align-center justufy-center">
             <EmployeeTable
-              data={data}
+              data={filteredData}
               start={indexOfFirstEntry}
               end={indexOfLastEntry}
             />
